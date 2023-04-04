e0035ab6d30704e89ed6d5600af56d7fc86fadc3
Madigan Carroll <Mac18@illinois.edu>
2023-04-04 02:47:32 -0500

    transmitter functions mostly done, might want some wrapper functions to set specific properties but it's in working condition now

--- Code/main/trans.c
+++ Code/main/trans.c
@@ -5,8 +5,6 @@
 static uint8_t trans_active = 0;
 
 void trans_command_full(uint8_t command, uint8_t *args, size_t num_args, uint16_t delay, uint8_t *response, size_t resp_len){
-    
-    size_t write_len = num_args;
 
     /*send the command*/
     i2c_register_write(TRANS_ADDR, command, args, num_args);
@@ -16,7 +14,6 @@ void trans_command_full(uint8_t command, uint8_t *args, size_t num_args, uint16_
 
     //recieve the response
     i2c_address_only_read(TRANS_ADDR, response, resp_len);
-
 }
 
 void trans_command_write(uint8_t command, uint8_t *args, size_t num_args){
@@ -70,9 +67,10 @@ void trans_power_up_std(uint8_t *response){
 
 }
 
-void trans_freq(uint16_t freq){
-/*set up the parameters*/
+void trans_set_freq_full(uint16_t freq){
+    /*set up the parameters*/
     uint8_t cmd = 0x30;
+
     uint8_t arg1 = 0x0;
     uint8_t arg2 = freq >> 8;
     uint8_t arg3 = freq & 0xff;
@@ -81,8 +79,123 @@ void trans_freq(uint16_t freq){
     
     uint16_t delay = 101; //change this to what is appropriate
 
+    unit8_t response;
     size_t resp_len = 1;
 
-    trans_command_full(cmd, args, num_args, delay, response, resp_len);
+    trans_command_full(cmd, args, num_args, delay, &response, resp_len);
 }
 
+void trans_set_freq_write(uint16_t freq){
+    /*set up the parameters*/
+    uint8_t cmd = 0x30;
+
+    uint8_t arg1 = 0x0;
+    uint8_t arg2 = freq >> 8;
+    uint8_t arg3 = freq & 0xff;
+    size_t num_args = 3;
+    uint8_t args[] = {arg1,arg2,arg3};
+    
+    trans_command_write(cmd, args, num_args);
+}
+
+void trans_set_power_write(uint8_t power){
+    /*set up the parameters*/
+    uint8_t cmd = 0x31;
+
+    uint8_t arg1 = 0;
+    uint8_t arg2 = 0;
+    uint8_t arg3 = power;
+    uint8_t arg4 = 0;
+    size_t num_args = 4;
+    uint8_t args[] = {arg1,arg2,arg3,arg4};
+    
+    uint16_t delay = 21; //change this to what is appropriate
+    
+    trans_command_write(cmd, args, num_args);
+}
+
+void trans_get_int_status(uint8_t *response){
+    /*set up parameters*/
+    uint8_t cmd = 0x14;
+    size_t num_args = 0;
+    uint8_t args[] = NULL;
+    
+    uint16_t delay = 1;
+
+    size_t resp_len = 1;
+
+    trans_command_full(cmd, args, num_args, delay, response, num_args);
+}
+
+uint8_t trans_get_int_status(void){
+    /*set up parameters, command and args*/
+    uint8_t cmd = 0x14;
+    size_t num_args = 0;
+    uint8_t args[] = NULL;
+    
+    /*define delay*/
+    uint16_t delay = 1;
+
+    /*set up parameters, response*/
+    uint8_t response;
+    size_t resp_len = 1;
+
+    /*send the command, get the response*/
+    trans_command_full(cmd, args, num_args, delay, &response, num_args);
+
+    return response;
+}
+
+void trans_tune_status(uint8_t *response){
+    /*set up parameters, command and args*/
+    uint8_t cmd = 0x33;
+    uint8_t arg1 = 0x1;
+    size_t num_args = 1;
+    uint8_t args[] = {arg1};
+    
+    /*define delay*/
+    uint16_t delay = 1;
+
+    /*set up parameters, response*/
+    size_t resp_len = 8;
+
+    /*send the command, get the response*/
+    trans_command_full(cmd, args, num_args, delay, &response, num_args);
+}
+
+void trans_set_property(uint16_t prop, uint16_t val, uint8_t *response){
+    /*set up parameters, command and args*/
+    uint8_t cmd = 0x12;
+    uint8_t arg1 = 0x0;
+    uint8_t arg2 = prop >> 8;
+    uint8_t arg3 = prop & 0xff;
+    uint8_t arg4 = val >> 8;
+    uint8_t arg5 = val & 0xff;
+    size_t num_args = 5;
+    uint8_t args[] = {arg1, arg2, arg3, arg4, arg5};
+    
+    /*define delay*/
+    uint16_t delay = 11;
+
+    /*set up parameters, response*/
+    size_t resp_len = 1;
+
+    /*send the command, get the response*/
+    trans_command_full(cmd, args, num_args, delay, &response, num_args);
+}
+
+void trans_set_property_write(uint16_t prop, uint16_t val, uint8_t *response){
+    /*set up parameters, command and args*/
+    uint8_t cmd = 0x12;
+    uint8_t arg1 = 0x0;
+    uint8_t arg2 = prop >> 8;
+    uint8_t arg3 = prop & 0xff;
+    uint8_t arg4 = val >> 8;
+    uint8_t arg5 = val & 0xff;
+    
+    size_t num_args = 5;
+    uint8_t args[] = {arg1, arg2, arg3, arg4, arg5};
+
+    /*send the command,*/
+    trans_command_write(cmd, args, num_args);
+}
--- Code/main/trans.h
+++ Code/main/trans.h
@@ -31,8 +31,62 @@ void trans_init(void);
 void trans_power_up_std(uint8_t *response);
 
 /**
- * @brief Change transmit frequency
+ * @brief Change transmit frequency, write and read response
  * 
  * @param freq New frequency in 10's of kHz. 101.1 MHz = 10110d = 0x277E
  */
-void trans_freq(uint16_t freq);
+void trans_set_freq_full(uint16_t freq);
+
+/**
+ * @brief Change transmit frequency, ony writes
+ * 
+ * @param freq New frequency in 10's of kHz. 101.1 MHz = 10110d = 0x277E
+ */
+void trans_set_freq_write(uint16_t freq);
+
+/**
+ * @brief Change transmit frequency, ony writes
+ * 
+ * @param power sets power in dBuV, range 88-115
+ */
+void trans_set_power_write(uint8_t power);
+
+/**
+ * @brief gets the interrupt satatus, includes delay, 1ms
+ * can only be sent during powerup mode
+ * 
+ * @param *response the response bit
+ */
+void trans_get_int_status(uint8_t *response);
+
+/**
+ * @brief gets the interrupt satatus, includes delay, 1ms,
+ * returns the status byte,
+ * can only be sent during powerup mode
+ */
+uint8_t trans_get_int_status(void);
+
+/**
+ * @brief tells what frequency and power level we 
+ * are currently transmitting
+ * 
+ * @param *response the response bit
+ */
+void trans_tune_status(uint8_t *response);
+
+/**
+ * @brief sets a property, gets status byte
+ * 
+ * @param *prop the property to set
+ * @param *val the value to set the property to
+ * @param *response the response bit
+ */
+void trans_set_property(uint16_t prop, uint16_t val, uint8_t *response);
+
+/**
+ * @brief sets a property, sends command only
+ * 
+ * @param *prop the property to set
+ * @param *val the value to set the property to
+ */
+void trans_set_property_write(uint16_t prop, uint16_t val);
